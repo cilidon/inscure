826f5669b76dba6405a9f9913137cdaaf8be0807
Mandar Mhatre <63152566+cilidon@users.noreply.github.com>
2020-06-16 21:46:35 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+email address: <?php echo $_POST["email"]; ?>
+phone number:<?php echo $_POST["phone number"]; ?>
+date of birth:<?php echo $_POST["date of birth"]; ?>
+password:<?php echo $_POST["password"]; ?>
+</body>
+</html>
---
+++ validate.js
@@ -0,0 +1,15 @@
+function validate()
+{
+    var username = document.getElementsByName("username");
+    var password = document.getElementsByName("password");
+    
+    if(username.value =="" || password.value=="" )
+        {
+            alert("Can't Be left blank")
+        }
+    
+    else
+    {
+        
+    }
+}
